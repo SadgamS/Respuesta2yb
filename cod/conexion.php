93668ac89a922f3ed9a38642c4d1f68be34bafa2
Brayan Dennis <brayan010.ba@gmail.com>
2020-10-19 20:19:01 -0400

    Modificacion de la conexion

--- cod/conexion.php
+++ cod/conexion.php
@@ -1,5 +1,5 @@
 <?php
     //Archivo para la conexion a la base de datos 
-    $conn = mysqli_connect("localhost","usuarioP","123456");
-    mysqli_select_db($conn, "academico1");
+    $conn = mysqli_connect("localhost","root","");
+    mysqli_select_db($conn, "academico");
 ?>
